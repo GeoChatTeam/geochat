f1b4435adeeee8a0d263623130503818cb3e56dc
jwolfman <jawjawbinx@yahoo.com>
2013-11-06 19:37:49 -0500

    Got tabs working, need to do a lot of CSS stuff.

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', chat.index);
+app.get('/chat',chat.chat);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/chat.js
+++ routes/chat.js
@@ -1,3 +1,6 @@
 exports.index = function(req, res){
 	res.render('home.ejs', {title: 'geochat'});
 };
+exports.chat=function(req,res){
+    res.render('chat.ejs',{title:'geochat'});
+}
