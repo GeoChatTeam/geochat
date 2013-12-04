f398d4f51982fa9b9ce85acf3aa09f84a2a50134
jwolfman <jawjawbinx@yahoo.com>
2013-12-04 01:23:32 -0500

    Tabs can be closed.

--- public/js/NotificationClient.js
+++ public/js/NotificationClient.js
@@ -4,10 +4,6 @@ function displayNotification(type, message){
 		error: "color: red; font-weight: italic;", 
 		neutral: "font-weight: bold;"
 	};
-	
-	jQuery('#chat-none').append('<span style="' + styles[type] +'">' + message + '</span><br />');
-	var objDiv = document.getElementById("tabs");
-	objDiv.scrollTop = objDiv.scrollHeight;
 }
 
 socket.on('notification', function(data){
--- public/js/TabClient.js
+++ public/js/TabClient.js
@@ -3,11 +3,19 @@ function makeTab(id,name){
         url: ('/tab/' + id),
         dataType: 'html',
         success: function(data){
+            if(jQuery('#tabs-'+id).length!=0){
+                return;
+            }
             jQuery('#tabs').append(data);
-            jQuery('#tab_list').append('<li><a href="#tabs-'+id+'">'+name+'</a></li>');
+            jQuery('#tab_list').append('<li id="link-'+id+'"><a href="#tabs-'+id+'">'+name+'<button style="width: 20px; height: 20px; line-height: 0;padding: 2" class="close" data-id="'+id+'">X</button></a></li>');
             $('#tabs').tabs('refresh');
         }
     })
+}
+function removeTab(id){
+    jQuery("#tabs-"+id).remove();
+    jQuery("#link-"+id).remove();
+    $('#tabs').tabs('refresh');
 }
 
 jQuery(document).on('click', '#roomList li', function(e){
